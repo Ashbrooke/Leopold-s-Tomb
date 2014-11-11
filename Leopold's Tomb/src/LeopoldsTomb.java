c1e2830de44d735858a0292577b844a97b3bb915
Ashbrooke <axeashbrooke@gmail.com>
2014-11-10 22:36:28 -0500

    Game is now fully functional. Code is somewhat convoluted because I wanted practice with different ways to call variables and methods. Could afford to have output text formatted.

--- Leopold's Tomb/src/LeopoldsTomb.java
+++ Leopold's Tomb/src/LeopoldsTomb.java
@@ -2,8 +2,9 @@
 public class LeopoldsTomb {
 	
 	boolean isRunning = true;
-	int gameStage = 0;
-	int currentRoom = 0;
+	static public int gameState = 0;
+	static int currentRoom = 0;
+	private String inventory = "Nothing";
 	String[] rooms = {"Tomb Entrance", "Main Hall", "Altar Room", "Flooded Room", "Tomb"};
 	
 	public LeopoldsTomb(){
@@ -13,6 +14,9 @@ public class LeopoldsTomb {
 		System.out.println("---------------------------");
 	}
 	
+	public static int getCurrentRoom(){
+		return currentRoom;
+	}
 	
 	public String getRoom(){
 		String room = rooms[currentRoom];
@@ -21,6 +25,36 @@ public class LeopoldsTomb {
 	
 	public void changeRoom(int nextRoom){
 		 currentRoom = nextRoom;
+	}
+
+
+	public String getInventory() {
+		return inventory;
+	}
+
+
+	public void addInventory(String item) {
+		inventory = item;
+	}
+
+
+	public static String getRoomDescription() {
+		String description = "";
+		switch (currentRoom) {
+        	case 0:  	description = "There is nothing interesting here, go on inside!";
+        				break;
+        	case 1:		description = "You walk inside of a large room with four support pillars. Engraved into the pillars are images of ships and large sea monsters.";
+        				break;
+        	case 2:		description = "The room is empty save a stone alter with the words 'The most important thing required for a well built ship.'";
+        				break;
+        	case 3:		if(gameState >= 3){
+        					description = "A large grate in the floor has drained all of the water.";
+           				}else{
+        					description = "A wall of water falls from the ceiling. Any attemt to pass through just washes you back to the door.";
+        				}
+        				break;
+		}
+		return description;
 	}
 
 	
--- Leopold's Tomb/src/Main.java
+++ Leopold's Tomb/src/Main.java
@@ -4,13 +4,15 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args){
+		boolean playerWin = false;
 		Scanner scannerInput = new Scanner(System.in);
 		LeopoldsTomb GameInstance = new LeopoldsTomb();
 		
-		
+		GameLoop:
 		while(GameInstance.isRunning){
 			
 			System.out.println("You are in the " + GameInstance.getRoom());
+			System.out.println(LeopoldsTomb.getRoomDescription());
 			System.out.println("What would you like to do?");
 			System.out.println("\t 1 - Move to a new room");
 			System.out.println("\t 2 - Search the " + GameInstance.getRoom());
@@ -24,7 +26,7 @@ public class Main {
 				scannerInput.close();
 				break;
 			}else if(input.equals("1")){
-				switch (GameInstance.currentRoom) {
+				switch (LeopoldsTomb.getCurrentRoom()) {
 		         case 0:  	nextRoom = 1;
 		                  	break;
 		         case 1:  	System.out.println("Which room would you like to enter?");
@@ -37,14 +39,17 @@ public class Main {
 		         			}else if(input.equals("2")){
 		         				nextRoom = 3;
 		         			}else if(input.equals("3")){
-		         				if(GameInstance.gameStage == 4){
+		         				if(LeopoldsTomb.gameState == 4){
 		         					nextRoom = 4;
+		         					playerWin = true;
+		         					GameInstance.isRunning = false;
+		         					break GameLoop;
 		         				}else{
-		         					nextRoom = GameInstance.currentRoom;
+		         					nextRoom = LeopoldsTomb.getCurrentRoom();
 		         					System.out.println("The Tomb doors are locked firmly in place! You can't enter.");
 		         				}
 		         			}else{
-		         				nextRoom = GameInstance.currentRoom;
+		         				nextRoom = LeopoldsTomb.getCurrentRoom();
 		         				System.out.println("You can't go there, it's not a valid destination!");
 		         			}
 		         			break;
@@ -62,14 +67,50 @@ public class Main {
 				
 				GameInstance.changeRoom(nextRoom);
 			}else if(input.equals("2")){
-				System.out.println("You search the room!");
+				if((LeopoldsTomb.getCurrentRoom() == 1)){
+					if(LeopoldsTomb.gameState == 0){
+						LeopoldsTomb.gameState = 1;
+						GameInstance.addInventory("Sacrificial Dagger");
+						System.out.println("You found a sacrificial dagger! It has been added to your inventory.");
+					}else{
+						System.out.println("You search the room thoroughly but find nothing of use.");
+					}
+				}else if(LeopoldsTomb.getCurrentRoom() == 2){
+					System.out.println("The only object here is the altar, and you can't carry that.");
+				}else if(LeopoldsTomb.getCurrentRoom() == 3){
+					if(LeopoldsTomb.gameState <= 2){
+						System.out.println("If you searched the room now you would surely drown.");
+					}else{
+						LeopoldsTomb.gameState = 4;
+						System.out.println("A large lever is located on the back wall of the room. You pull it and hear doors open.");
+					}
+				}else{
+					System.out.println("You find nothing usefull.");
+				}
 			}else if(input.equals("3")){
-				System.out.println("You check your inventory!");
+				if(LeopoldsTomb.getCurrentRoom() == 2 && LeopoldsTomb.gameState == 1){
+					LeopoldsTomb.gameState = 3;
+					GameInstance.addInventory("Bloody Sacrificial Dagger");
+					System.out.println("You use the dagger to prick your finger. A drop of blood falls onto the altar, representing the hard work that goes into shipbuilding.");
+					System.out.println("A panel above the altar slides away and reveals a large valve. As you turn the valve the rushing sound of water stops.");
+				}else{
+					System.out.println("You look inside of your adventurers bag and see:");
+     				System.out.println(GameInstance.getInventory());
+				}
 			}else{
 				System.out.println("Not a valid command");
 			}
 			
 		}
+		
+		scannerInput.close();
+		if(playerWin){
+			System.out.println("Congratulations! You have entered the Tomb! You go on to be the greatest archaeologist in history!");
+		}else{
+			System.out.println("You have exited the game.");
+		}
+		
+		System.out.println("Thanks for playing!");
 		
 	}
 	
